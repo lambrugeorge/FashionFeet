484eeeed2a3e0f0878538100b2d4d546fecbf434
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-24 22:16:08 +0300

    update

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Nav.css'
+import {FiHeart} from 'react-icons/fi';
+import {AiOutlineShoppingCart, AiOutlineUerAdd} from 'react-icons/ai';
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 
 function Nav() {
   return (
-    <div>Nav</div>
+   <nav>
+       <div className='nav-container'>
+        <input type="text" 
+        className="search-input"
+        placeholder = "Enter your search shoes."
+        />
+       </div>
+
+       <div className='profile-container'>
+       <a href='#'>
+       <FiHeart className="nav-icons"/>
+       </a>
+
+       <a href='#'>
+       <AiOutlineShoppingCart className="nav-icons"/>
+       </a>
+
+       <a href='#'>
+       <AiOutlineUerAdd className="nav-icons"/>
+       </a>
+       </div>
+   </nav>   
   )
 }
 
