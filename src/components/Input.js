3260b45952bec43aeaeaa368a5246be2d176feb7
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-31 22:38:46 +0300

    created price component refactor

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Input() {
+function Input({handleChange, value, title, name, color}) {
   return (
     <label className='sidebar-label-container'>
-    <input type="radio" name="test2"/>
-    <span className='checkmark'></span>All
+    <input onChange={handleChange} type="radio" value={value} name={name}/>
+    <span className='checkmark' style={{backgroundColor: color}}></span>{title}
   </label>
   );
 }
