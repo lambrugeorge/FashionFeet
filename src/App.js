c1db8ce3a56fc97b6836590272e3d62997a64361
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-31 17:49:40 +0300

    passing functionality to each component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,70 @@ import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
-import { useState } from "react";
 // database
-import products form './db/data';
-
+import products from './db/data'
+import Card from "./components/Card";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null)
+  const [query, setQuery] = useState("")
 
-  const [query, setQuery] = event => {
-    setQuery(event.target.value)
-  }
+  const handleInputChange = (event) => {
+  setQuery(event.target.value);
+ }
 
-  const filteredItems = products.filter(products => product.title.
-    toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+
+  const filteredItems = products.filter(product => 
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
   );
 
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
 
+
+  const handleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products
+
+    if (query) {
+      filteredProducts = filteredItems
+    }
+
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+      ({category, color, company,
+        newPrice, title}) => 
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected 
+      );
+    }
+    return filteredProducts.map(
+      ({img, title, star, reviews, newPrice, prevPrice}) => {
+      <Card 
+      key={Math.random()}
+      img = {img}
+      title = {title}
+      star = {star}
+      reviews = {reviews}
+      newPrice = {newPrice}
+      prevPrice = {prevPrice}
+      />
+    })
+  }
+
+const result = filteredData(products, selectedCategory, query)
+  
   return <>
-  <Sidebar />
+  <Sidebar handleChange = {handleChange} />
   <Navigation />
   <Recommended />
   <Products />
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,15 +4,16 @@ import Price from './Price/Price'
 import Colors from './Colors/Colors'
 import Category from './Category/Category'
 
-function Sidebar() {
+function Sidebar({handleChange}) {
+
   return <>
      <section className='sidebar' >
        <div className='logo-container'>
              <h1>🛒</h1> 
        </div>
-       <Category />
-       <Price />
-       <Colors />
+       <Category handleChange = {handleChange}/>
+       <Price handleChange = {handleChange}/>
+       <Colors handleChange = {handleChange}/>
      </section>  
   </>
 }
